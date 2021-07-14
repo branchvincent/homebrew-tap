d2321b1b767cb77f0ee15c63c8814cc96d769941
Branch Vincent <19800529+branchvincent@users.noreply.github.com>
2021-07-14 04:51:44 +0000

    powerlevel10k: add 1.15.0 bottle.

--- Formula/powerlevel10k.rb
+++ Formula/powerlevel10k.rb
@@ -4,6 +4,11 @@ class Powerlevel10k < Formula
   url "https://github.com/romkatv/powerlevel10k/archive/v1.15.0.tar.gz"
   sha256 "3482ae9b876890b78dd2567066cf5ceecd37eb2117c12248106710655a7dc1dc"
 
+  bottle do
+    root_url "https://github.com/branchvincent/homebrew-tap/releases/download/powerlevel10k-1.15.0"
+    sha256 cellar: :any_skip_relocation, catalina: "10e6544e42deb1cce0f5541de5dd95193ad482bfa506cf59e075a4e043e99fc8"
+  end
+
   depends_on "zsh"
 
   def install
