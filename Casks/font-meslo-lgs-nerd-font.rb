c08bec39c1d7a43c448f11db308802a48840b4f8
Branch Vincent <branchevincent@gmail.com>
2021-07-15 17:41:09 -0400

    font-meslo-lgs-nerd-font (new font) (#5)

---
+++ Casks/font-meslo-lgs-nerd-font.rb
@@ -0,0 +1,13 @@
+cask "font-meslo-lgs-nerd-font" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/romkatv/powerlevel10k-media.git"
+  name "MesloLGS Nerd Font families (Meslo LG)"
+  homepage "https://github.com/romkatv/powerlevel10k-media"
+
+  font "MesloLGS NF Bold Italic.ttf"
+  font "MesloLGS NF Bold.ttf"
+  font "MesloLGS NF Italic.ttf"
+  font "MesloLGS NF Regular.ttf"
+end
