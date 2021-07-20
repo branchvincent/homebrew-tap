9d3aca8359087083bc5d93f605ef4a4c78a586da
Branch Vincent <branchevincent@gmail.com>
2021-07-19 23:12:59 -0400

    littlecheck: simplify head

--- Formula/littlecheck.rb
+++ Formula/littlecheck.rb
@@ -5,14 +5,11 @@ class Littlecheck < Formula
   homepage "https://github.com/ridiculousfish/littlecheck"
   url "https://github.com/ridiculousfish/littlecheck/archive/e6d56afd8dd14d8ce69d69326353f77d09e71c38.tar.gz"
   sha256 "514c15df1a193d377582f15b965bfa9a87ed00e718dda3d3f806f57db16bdb57"
+  head "https://github.com/ridiculousfish/littlecheck.git"
 
   bottle do
     root_url "https://github.com/branchvincent/homebrew-tap/releases/download/littlecheck-38"
     sha256 cellar: :any_skip_relocation, catalina: "48ae757282649cbe2ef6775fcc421ce58746f3f529ac50c6a0c00341bfc0f7f7"
-  end
-
-  head do
-    url "https://github.com/ridiculousfish/littlecheck.git"
   end
 
   depends_on "python@3.9"
