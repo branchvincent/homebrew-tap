8b15caa78782c54a6e57336cf5b23708f23a0252
Branch Vincent <branchevincent@gmail.com>
2021-07-19 23:16:33 -0400

    powerlevel10k: simplify head

--- Formula/powerlevel10k.rb
+++ Formula/powerlevel10k.rb
@@ -3,14 +3,11 @@ class Powerlevel10k < Formula
   homepage "https://github.com/romkatv/powerlevel10k"
   url "https://github.com/romkatv/powerlevel10k/archive/v1.15.0.tar.gz"
   sha256 "3482ae9b876890b78dd2567066cf5ceecd37eb2117c12248106710655a7dc1dc"
+  head "https://github.com/romkatv/powerlevel10k.git"
 
   bottle do
     root_url "https://github.com/branchvincent/homebrew-tap/releases/download/powerlevel10k-1.15.0"
     sha256 cellar: :any_skip_relocation, catalina: "10e6544e42deb1cce0f5541de5dd95193ad482bfa506cf59e075a4e043e99fc8"
-  end
-
-  head do
-    url "https://github.com/romkatv/powerlevel10k.git"
   end
 
   depends_on "zsh"
