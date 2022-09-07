f482c3eb8510b8905ce5b9aaa981f61c4295a041
Branch Vincent <branchevincent@gmail.com>
2022-09-07 18:31:46 -0400

    bun 0.1.11

--- Formula/bun.rb
+++ Formula/bun.rb
@@ -1,9 +1,9 @@
 class Bun < Formula
   desc "Fast all-in-one JavaScript runtime"
   homepage "https://bun.sh"
-  url "https://github.com/oven-sh/bun/releases/download/bun-v0.1.10/bun-darwin-aarch64.zip"
-  version "0.1.10"
-  sha256 "f37a77ed4c503540dcfebbd39261b55c9c5ccc5599d8edcf502752949a4b2dbe"
+  url "https://github.com/oven-sh/bun/releases/download/bun-v0.1.11/bun-darwin-aarch64.zip"
+  version "0.1.11"
+  sha256 "475ec66b80ba3d2c5f2f56f3e88063c747581665fdde2d32f2eab11b579929de"
   license "MIT"
 
   livecheck do
@@ -14,8 +14,8 @@ class Bun < Formula
   depends_on :macos
 
   resource "completions" do
-    url "https://github.com/oven-sh/bun/archive/refs/tags/bun-v0.1.10.tar.gz"
-    sha256 "ea3cdfa538a155341adedfb395c39596a6b2ba0c60ff6250e47aa381b99af775"
+    url "https://github.com/oven-sh/bun/archive/bun-v0.1.11.tar.gz"
+    sha256 "e7668660b4c1608d7eb481a2a12cd7f427597c7be0a42bc8ab068ca0a3953a9e"
   end
 
   def install
